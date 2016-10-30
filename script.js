1ea7eb4fe61e1a7ca6a8c2920b624959fa919ea3
allisonlu <allisonlu820@gmail.com>
2016-10-30 16:00:07 -0700

    added basic navbar, added placeholders on multimedia page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("navMenu").innerHTML =
+'<ul>' +
+'<li><a href="election16.html">Home</a></li>' +
+'<li><a href="#">Endorsements</a></li>' +
+'<li><a href="#">Opinion</a></li>' +
+'<li><a href="#">News</a></li>' +
+'<li><a href="multimedia.html">Multimedia</a></li>' +
+'</ul>';
