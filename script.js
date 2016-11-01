1d6e5259e909af1774f4638013a0a1ff96774a8f
allisonlu <allisonlu820@gmail.com>
2016-10-31 23:29:37 -0700

    nav bar coming together!
    will fix that line soon

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("navMenu").innerHTML =
-'<ul>' +
-'<li><a href="election16.html">Home</a></li>' +
-'<li><a href="#">Endorsements</a></li>' +
-'<li><a href="#">Opinion</a></li>' +
-'<li><a href="#">News</a></li>' +
-'<li><a href="multimedia.html">Multimedia</a></li>' +
+'<ul class="nav nav-tabs">' +
+'<li role="presentation" class="active"><a href="election16.html">Home</a></li>' +
+'<li role="presentation"><a href="#">Endorsements</a></li>' +
+'<li role="presentation"><a href="#">Opinion</a></li>' +
+'<li role="presentation"><a href="#">News</a></li>' +
+'<li role="presentation"><a href="multimedia.html">Multimedia</a></li>' +
 '</ul>';
